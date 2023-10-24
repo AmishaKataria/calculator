51a364cae17c234881a23e0c8ca86b20843c901d
@AmishaKataria <amishakataria2110@gmail.com>
2023-10-25 00:22:31 +0530

    added calculator files

---
+++ calculator.js
@@ -0,0 +1,8 @@
+ let currentDisplay='';
+ document.querySelector('#display').value=currentDisplay;
+
+ document.querySelector("html").addEventListener("click", () => {
+    alert("Ouch! Stop poking me!");
+  });
+  
+ 
